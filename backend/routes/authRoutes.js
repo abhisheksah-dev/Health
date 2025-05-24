c03c4d83d248841b86c9d34ae841037576c98c4f
atulchakwa <atulchakwa41@gmail.com>
2025-05-24 13:55:44 +0530

    Authentication is complete

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { register, login,refreshToken } from '../controller/authController.js'
+
+import authMiddleware from '../middleware/authmiddleware.js';
+import e from 'express';
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.post('/refresh-token', refreshToken);
+router.get('/profile', authMiddleware, (req, res) => {
+    res.json(req.user);
+});
+
+export default router;
